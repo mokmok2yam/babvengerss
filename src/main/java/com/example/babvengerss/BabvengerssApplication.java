1ba30f604aaf096879c86f8c148c22bfafeee2fd
HanJeongmok <wjdahe1845@gmail.com>
2025-09-15 15:00:17 +0900

    firstd-commit

---
+++ src/main/java/com/example/babvengerss/BabvengerssApplication.java
@@ -0,0 +1,13 @@
+package com.example.babvengerss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabvengerssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabvengerssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/babvengerss/BabvengerssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babvengerss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabvengerssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
